e5d968899d041f8b358d97bbd0ae9682b4c314a6
Adam Schiavone <adam8797@me.com>
2015-12-31 12:24:15 -0500

    Added Project Files

---
+++ Drip.Gui/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Drip.Gui
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Console.WriteLine("Drip -  Driving Robots over IP");
+            Console.WriteLine("(totally not a forced acronym)");
+            Console.WriteLine("(c) Adam Schiavone 2015, All rights reserved");
+            Console.WriteLine("Written for Illinois Robotics");
+
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
