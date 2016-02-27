ba3b57db1cd3f50a0a1c7d4612a1e1dfc9a03388
dnull7 <wdrn07@gmail.com>
2016-02-27 10:38:27 -0600

    various changes
    small insignificant stuff.  Im only committing to get things up to date

--- Drip.Gui/CustomLogic/IllinoisRobotClient.cs
+++ Drip.Gui/CustomLogic/IllinoisRobotClient.cs
@@ -35,7 +35,7 @@ namespace Drip.Gui.CustomLogic
         public bool SkipFrame = false;
 
 
-        const double ThrusterMaxValue = 100;
+        const double ThrusterMaxValue = 5;
 
         public override void SendFrame(RobotFrame frame)
         {
